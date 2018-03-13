e424d269a04c9087b38b1e9456f751514be7d9cc
Nur M Shahir <nmshahir@email.unc.edu>
2018-03-13 10:42:17 -0400

    Updated MR_steps.R
    Updated test script with minimal needed

--- MR_Steps.R
+++ MR_Steps.R
@@ -1,25 +1,21 @@
 library(TwoSampleMR)
 ### Pick metabolite of choice
-
 exposure_dat <- function(gwas_filename,metabolite) {
   metabolite_subset <- subset(gwas_filename, grepl(metabolite,Phenotype))
-  metabolite.data <- format_data(metabolite_subset)
-  # metabolite.data <- read_exposure_data(filename = metabolite_subset,
-  #                    sep = ",",
-  #                    snp_col = "rsid",
-  #                    beta_col = "effect",
-  #                    se_col = "SE",
-  #                    effect_allele_col = "a1",
-  #                    other_allele_col = "a2",
-  #                    eaf_col = "a1_freq",
-  #                    pval_col = "p-value",
-  #                    units_col = "Units",
-  #                    gene_col = "Gene",
-  #                    samplesize_col = "n")
+  metabolite.data <- read_exposure_data(filename = metabolite_subset,
+                      sep = ",",
+                      snp_col = "rsid",
+                      beta_col = "effect",
+                      se_col = "SE",
+                      effect_allele_col = "a1",
+                      other_allele_col = "a2",
+                      eaf_col = "a1_freq",
+                      pval_col = "p-value")
   # metabolite.data$exposure <- metabolite
   return(metabolite.data)
 }
 
+### Pick disease of choice
 disease_out_dat <- function(disease_GWAS_filename,metabolite_snp_dat) {
   disease_outcome_dat <- read_outcome_data(
     snps = metabolite_snp_dat$SNP,
@@ -31,10 +27,7 @@ disease_out_dat <- function(disease_GWAS_filename,metabolite_snp_dat) {
     effect_allele_col = "reference_allele",
     other_allele_col = "other_allele",
     eaf_col = "ref_allele_frequency",
-    pval_col = "pvalue",
-    units_col = NA,
-    gene_col = NA,
-    samplesize_col = NA
+    pval_col = "pvalue"
   )
   disease_outcome_dat$outcome <- "CAD"
   return(disease_outcome_dat)
@@ -44,10 +37,3 @@ dat <- harmonise_data(
   exposure_dat = bmi_exp_dat, 
   outcome_dat = format.overlap.bmi.cad
 )
-
-##### EXAMPLE RUN
-exposure_data <- exposure_dat(metab_qtl_input,metabolite = "Glutamine")
-
-#Prune step from Nick??? ###
-outcome_data <- disease_out_dat(disease_gwas,pruned_snp_list)
-dat <- harmonise_data(exposure_dat=exposure_data, outcome_dat = outcome_data)
