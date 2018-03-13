046ae41692a036b31c254fc007ea66228d43d103
Nur M Shahir <nmshahir@email.unc.edu>
2018-03-13 17:27:16 -0400

    Update MR_backend.R
    Streamlined because I was making it too complicated

--- MR_backend.R
+++ MR_backend.R
@@ -1,39 +1,55 @@
 library(TwoSampleMR)
 ### Pick metabolite of choice
-exposure_dat <- function(gwas_filename,metabolite) {
-  metabolite_subset <- subset(gwas_filename, grepl(metabolite,Phenotype))
-  metabolite.data <- read_exposure_data(filename = metabolite_subset,
-                      sep = ",",
-                      snp_col = "rsid",
-                      beta_col = "effect",
-                      se_col = "SE",
-                      effect_allele_col = "a1",
-                      other_allele_col = "a2",
-                      eaf_col = "a1_freq",
-                      pval_col = "p-value")
-  # metabolite.data$exposure <- metabolite
-  return(metabolite.data)
-}
+#serum_file <- "200_serum.txt"
+#glutamine_path <- "glutamine.csv"
+#CAD_path <- "CARDIoGRAM_GWAS_RESULTS.txt"
+#CAD_studies <- "CAD_example_studies.csv"
+#Get path for metab_qtl_path
+#They're going to need to be split in advanced
+metab_qtl_path <- ""
+disease_GWAS_filename <- "" #CardiogramC4D thing?#
+metabolite_data <- read_exposure_data(metab_qtl_path,
+                       sep = ",",
+                       snp_col = "SNP",
+                       beta_col = "beta.exposure",
+                       se_col = "se.exposure",
+                       eaf_col = "eaf.exposure",
+                       effect_allele_col = "effect_allele.exposure",
+                       other_allele_col = "other_allele.exposure",
+                       pval_col = "pval.exposure")
 
-### Pick disease of choice
-disease_out_dat <- function(disease_GWAS_filename,metabolite_snp_dat) {
-  disease_outcome_dat <- read_outcome_data(
-    snps = metabolite_snp_dat$SNP,
+metabolite_clump_data <- clump_data(metabolite_data)
+
+disease_outcome_dat <- read_outcome_data(
+    snps = metabolite_clump_data$SNP,
     filename = disease_GWAS_filename,
-    sep = "\t",
+    sep = ",",
     snp_col = "SNP",
-    beta_col = "log_odds",
-    se_col = "log_odds_se",
-    effect_allele_col = "reference_allele",
+    beta_col = "beta",
+    se_col = "se",
+    effect_allele_col = "effect_allele",
     other_allele_col = "other_allele",
-    eaf_col = "ref_allele_frequency",
-    pval_col = "pvalue"
+    eaf_col = "eaf",
+    pval_col = "pval"
   )
   disease_outcome_dat$outcome <- "CAD"
   return(disease_outcome_dat)
 }
 
+### Pick disease of choice
+disease_outcome_dat <- read_outcome_data(CAD_studies,
+    snps = metabolite_clump_data$SNP,
+    sep = ",",
+    snp_col = "SNP",
+    beta_col = "beta",
+    se_col = "se",
+    effect_allele_col = "effect_allele",
+    other_allele_col = "other_allele",
+    eaf_col = "eaf",
+    pval_col = "pval"
+  )
+
 dat <- harmonise_data(
-  exposure_dat = bmi_exp_dat, 
-  outcome_dat = format.overlap.bmi.cad
+  exposure_dat = metabolite_clump_data, 
+  outcome_dat = disease_outcome_dat
 )
