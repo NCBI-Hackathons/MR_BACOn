2b0367ae23fdb50b15d364d6f1ecb4b6c1a5cc1b
Nick Colaianni <ncolaian@dhcp-v880-0-00519.1Xwireless.unc.edu>
2018-03-14 16:59:36 -0400

    lower threshold

--- ShinyApp/backend_functions.R
+++ ShinyApp/backend_functions.R
@@ -29,7 +29,7 @@ perform_mr <- function(tissue, dataset, metid) {
   expos_data <- read_exposure_data(filename = paste(path,tissue,".txt", sep=""), id_col = "metID", sep = "\t")
   expos_data <- expos_data[expos_data$id.exposure == metabo,]
   expos_data <- clump_data(expos_data, clump_kb = 1000, clump_r2 = 0.8, clump_p1 = 1, clump_p2 = 1)
-  disease_outcome <- read_outcome_data(filename = paste(path,"cad.txt", sep=""), snps = expos_data$SNP, sep = "\t")
+  disease_outcome <- read_outcome_data(filename = paste(path,"cad_2.txt", sep=""), snps = expos_data$SNP, sep = "\t")
   harmed_data <- harmonise_data(exposure_dat = expos_data, outcome_dat = disease_outcome)
   return(harmed_data)
 }
