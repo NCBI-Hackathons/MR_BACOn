e758c99fbc30c181131af9c987821d06016de790
Yue Hao <haoyuethink@gmail.com>
2018-03-25 14:13:14 -0400

    Update parse_pathways.pl

--- Pathway_analysis/parse_pathways.pl
+++ Pathway_analysis/parse_pathways.pl
@@ -8,7 +8,6 @@ use strict;
 
 my($xmlfile, $readfh, $outfh, $outfile, $line, @array, $IDlist,
    $tempfile, %Metabolite, $metabid, %Subid, %Pathway, $path, $secondid
-
 );
 
 $xmlfile = $ARGV[0];
@@ -19,13 +18,12 @@ $outfile = 'Pathways_'.$tempfile.'.txt';
 $IDlist = 'SecondaryID_'.$tempfile.'.txt';
 $tempfile = 'temp_'.$tempfile.'.txt';
 
-
 `./nested_xml_parser.py $xmlfile > $tempfile`;
 
 open $readfh,'<',$tempfile or die "$!";
 
 while (!eof($readfh)) {
-	$line = <$readfh>;
+    $line = <$readfh>;
     $line =~ s/\r*\n*//g;
     if ($line =~ /^HMDB[0-9]+/) {
         @array = split /\t/, $line;
@@ -47,13 +45,13 @@ open $outfh,'>',$outfile or die "$!";
 
 print $outfh qq{Pathway\tHMDBID\tMetabolite\n};
 
-
 foreach $path (sort keys %Pathway){
     foreach $metabid (sort keys %{$Pathway{$path}}) {
         print $outfh qq{$path\t$metabid\t$Metabolite{$metabid}\n};
     }
 }
 close $outfh;
+
 open $outfh,'>',$IDlist or die "$!";
 print $outfh qq{HMDBID\tSecondaryID\n};
 
@@ -62,4 +60,4 @@ foreach $metabid (sort keys %Subid){
         print $outfh qq{$metabid\t$secondid\n};
     }
 }
-close$outfh;
+close $outfh;
